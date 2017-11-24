0d7f63b60808fae8ec9df05b4c6a410a3433716a
plwei <1172280308@qq.com>
2017-11-24 17:15:51 +0800

    修改部分内容

--- controlFile/WebRoot/common/pages/show/tag_ctrl.js
+++ controlFile/WebRoot/common/pages/show/tag_ctrl.js
@@ -1,4 +1,8 @@
 $(function(){
+	$('.header .item ul').each(function(i, n) {
+		var height = $(this).height();
+		$(this).parent().height(height);
+	})
 	$('.title td').on('click', function() {
 		$('.title td').removeClass('active');
 		$(this).addClass('active');
