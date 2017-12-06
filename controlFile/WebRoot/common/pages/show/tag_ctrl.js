30a83a61ec19ee2cd046b64eeff7914f6501c913
plwei <1172280308@qq.com>
2017-12-06 15:43:59 +0800

    2017/12/6 提交

--- controlFile/WebRoot/common/pages/show/tag_ctrl.js
+++ controlFile/WebRoot/common/pages/show/tag_ctrl.js
@@ -7,9 +7,24 @@ $(function(){
 		$('.subnav td').removeClass('active');
 		$(this).addClass('active');
 	})
+	$('input[name="bqjb"]').on('click', function() {
+		var value = $(this).val();
+		if(value == 1) {
+			$('#add > div').eq(1).hide();
+			$('#add > div').eq(2).hide();
+			$('#add > div').eq(3).hide();
+		} else if(value == 2) {
+			$('#add > div').eq(2).hide();
+			$('#add > div').eq(3).hide();
+		} else if(value == 3) {
+			$('#add > div').eq(3).hide();
+		}
+	})
 })
 
-
+function addInput() {
+	
+}
 
 
 
